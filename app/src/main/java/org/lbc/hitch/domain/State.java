fce824f0ca39b96979640caf547f9db054765a25
Kelby Sapien <sapikelby@gmail.com>
2016-06-11 17:09:19 -0500

    Chaned gradle dependency and State enum (added state code, name)

--- app/src/main/java/org/lbc/hitch/domain/State.java
+++ app/src/main/java/org/lbc/hitch/domain/State.java
@@ -6,58 +6,70 @@ package org.lbc.hitch.domain;
  * TODO write unit test
  */
 public enum State {
-    Alabama,
-    Alaska,
-    Arizona,
-    Arkansas,
-    California,
-    Colorado,
-    Connecticut,
-    Delaware,
-    Florida,
-    Georgia,
-    Hawaii,
-    Idaho,
-    Illinois,
-    Indiana,
-    Iowa,
-    Kansas,
-    Kentucky,
-    Louisiana,
-    Maine,
-    Maryland,
-    Massachusetts,
-    Michigan,
-    Minnesota,
-    Mississippi,
-    Missouri,
-    Montana,
-    Nebraska,
-    Nevada,
-    New_Hampshire,
-    New_Jersey,
-    New_Mexico,
-    New_York,
-    North_Carolina,
-    North_Dakota,
-    Ohio,
-    Oklahoma,
-    Oregon,
-    Pennsylvania,
-    Rhode_Island,
-    South_Carolina,
-    South_Dakota,
-    Tennessee,
-    Texas,
-    Utah,
-    Vermont,
-    Virginia,
-    Washington,
-    West_Virginia,
-    Wisconsin,
-    Wyoming;
+    ALABAMA("ALABAMA", "AL"),
+    ALASKA("ALASKA", "AK"),
+    ARIZONA("ARIZONA", "AZ"),
+    ARKANSAS("ARKANSAS","AR"),
+    CALIFORNIA("CALIFORNIA","CA"),
+    COLORADO("COLORADO", "CO"),
+    CONNECTICUT("CONNECTICUT","CT"),
+    DELAWARE("DELAWARE","DE"),
+    FLORIDA("FLORIDA","FL"),
+    GEORGIA("GEORGIA", "GA"),
+    HAWAII("HAWAII", "HI"),
+    IDAHO("IDAHO", "ID"),
+    ILLINOIS("ILLINOIS", "IL"),
+    INDIANA("INDIANA",	"IN"),
+    IOWA("IOWA","IA"),
+    KANSAS("KANSAS", "KS"),
+    KENTUCKY("KENTUCKY", "KY"),
+    LOUISIANA("LOUISIANA", "LA"),
+    MAINE("MAINE", "ME"),
+    MARYLAND("MARYLAND","MD"),
+    MASSACHUSETTS("MASSACHUSETTS", "MA"),
+    MICHIGAN("MICHIGAN", "MI"),
+    MINNESOTA("MINNESOTA", "MN"),
+    MISSISSIPPI("MISSISSIPPI", "MS"),
+    MISSOURI("MISSOURI", "MO"),
+    MONTANA("MONTANA", "MT"),
+    NEBRASKA("NEBRASKA", "NE"),
+    NEVADA("NEVADA", "NV"),
+    NEW_HAMPSHIRE("NEW HAMPSHIRE", "NH"),
+    NEW_JERSEY("NEW JERSEY", "NJ"),
+    NEW_MEXICO("NEW MEXICO", "NM"),
+    NEW_YORK("NEW YORK", "NY"),
+    NORTH_CAROLINA("NORTH CAROLINA", "NC"),
+    NORTH_DAKOTA("NORTH DAKOTA", "ND"),
+    OHIO("OHIO", "OH"),
+    OKLAHOMA("OKLAHOMA", "OK"),
+    OREGON("OREGON", "OR"),
+    PENNSYLVANIA("PENNSYLVANIA", "PA"),
+    RHODE_ISLAND("RHODE ISLAND", "RI"),
+    SOUTH_CAROLINA("SOUTH CAROLINA", "SC"),
+    SOUTH_DAKOTA("SOUTH DAKOTA", "SD"),
+    TENNESSEE("TENNESSEE", "TN"),
+    TEXAS("TEXAS", "TX"),
+    UTAH("UTAH", "UT"),
+    VERMONT("VERMONT","VT"),
+    VIRGINIA("VIRGINIA", "VA"),
+    WASHINGTON("WASHINGTON", "WA"),
+    WEST_VIRGINIA("WEST VIRGINIA", "WV"),
+    WISCONSIN("WISCONSIN", "WI"),
+    WYOMING("WYOMING","WY");
 
-    public String toString(State state) {
-        return state.name().replace("_", " ");
+    private String name;
+    private String code;
+
+    State(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
     }
 }
