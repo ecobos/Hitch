cf6fd4c4a6130cdc3579f418cbd594ae52db3470
Kelby Sapien <sapikelby@gmail.com>
2016-07-03 20:49:48 -0500

    Changed Trip class to conform to Firebase models

--- app/src/main/java/org/lbc/hitch/domain/Trip.java
+++ app/src/main/java/org/lbc/hitch/domain/Trip.java
@@ -1,33 +1,57 @@
 package org.lbc.hitch.domain;
 
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
 import java.io.Serializable;
+import java.sql.Date;
 
 /**
  * Created by Kelby on 11/13/2015.
  */
-public class Trip implements Serializable {
 
+@IgnoreExtraProperties
+public class Trip {
+
+    private long tripId;
     private String driver_id;
     private int passengerCount;
     private String departingDestination;
     private String finalDestination;
     private String departingTime;
-    private String cancellationTime = null;
+    private String cancellationTime;
+    private Date tripDate; // TODO: is this the date type we want
 
-    // TODO consider removing cancelledAt param since this is for a new trip
-    // TODO create setter for setting cancellationTime
-    public Trip(String driver, int passengers, String departingDest, String finalDest,
-                String departingTime, String cancelledAt) {
-        this.driver_id = driver;
-        this.passengerCount = passengers;
-        this.departingDestination = departingDest;
-        this.finalDestination = finalDest;
-        this.departingTime = departingTime;
-        this.cancellationTime = cancelledAt;
+    public Trip() {
+        // Default constructor required for calls to DataSnapshot.getValue(Post.class)
     }
 
-    public String getDriverByID() {
+    // TODO create setter for setting cancellationTime
+    public Trip(long tripId, String driver_id, int passengerCount, String departingDestination, String finalDestination, String departingTime, String cancellationTime, Date tripDate) {
+        this.tripId = tripId;
+        this.driver_id = driver_id;
+        this.passengerCount = passengerCount;
+        this.departingDestination = departingDestination;
+        this.finalDestination = finalDestination;
+        this.departingTime = departingTime;
+        this.cancellationTime = cancellationTime;
+        this.tripDate = tripDate;
+    }
+
+    public String getCancellationTime() {
+        return cancellationTime;
+    }
+
+    public long getTripId() {
+        return tripId;
+    }
+
+    public String getDriver_id() {
         return driver_id;
+    }
+
+    public int getPassengerCount() {
+        return passengerCount;
     }
 
     public String getDepartingDestination() {
@@ -42,7 +66,36 @@ public class Trip implements Serializable {
         return departingTime;
     }
 
-    public String getCancellationTime() {
-        return cancellationTime;
+    public Date getTripDate() {
+        return tripDate;
+    }
+
+    public void setCancellationTime(String cancellationTime) {
+        this.cancellationTime = cancellationTime;
+    }
+
+    public void setDepartingTime(String departingTime) {
+        this.departingTime = departingTime;
+    }
+
+    public void setDepartingDestination(String departingDestination) {
+        this.departingDestination = departingDestination;
+    }
+
+    public void setFinalDestination(String finalDestination) {
+        this.finalDestination = finalDestination;
+    }
+
+    @Exclude
+    @Override
+    public String toString() {
+        return "Trip{" +
+                "driver_id='" + driver_id + '\'' +
+                ", passengerCount=" + passengerCount +
+                ", departingDestination='" + departingDestination + '\'' +
+                ", finalDestination='" + finalDestination + '\'' +
+                ", departingTime='" + departingTime + '\'' +
+                ", cancellationTime='" + cancellationTime + '\'' +
+                '}';
     }
 }
